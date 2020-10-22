cffdbf8ef276462ccaf064a7a5df1cd81a49c37f
k5275645 <k5275645@naver.com>
2020-10-22 17:44:03 +0900

    almost

--- calculator/knh/index.js
+++ calculator/knh/index.js
@@ -1,3 +1,4 @@
+//선택자설정
 const calculator = document.querySelector('.calculator');
 const numBtns = calculator.querySelectorAll('.num');
 const opBtns = calculator.querySelectorAll('.op');
@@ -6,32 +7,31 @@ const equalBtn = calculator.querySelector('.equal');
 const resultShow = calculator.querySelector('.resultShow span');
 const processShow = calculator.querySelector('.process span');
 
-//
+//변수설정
 let result = 0;
+let tempResult = 0;
 let nextOp = 0;
 let afterOp = false;
-let isFirst = false;
 let op = "";
 let anyStr = "";
 
-
-//
+//확인용
 function processStack(any) {
     anyStr = anyStr + any.toString();
     printProcess(anyStr);
 }
 
-//
+//확인용
 function printProcess(anyStr) {
     processShow.innerText = anyStr;
 }
 
-//
+//출력
 function printResult(numStr) {
     resultShow.innerText = numStr;
 }
 
-//
+//화면에 쌓기
 function stack(num, cVal) {
     let numStr = num.toString();
     if (numStr === "0") {
@@ -43,14 +43,17 @@ function stack(num, cVal) {
     return numStr;
 }
 
-//
+//숫자처리
 function numChk(e) {
     e.preventDefault();
-    const cVal = e.target.textContent;
+    let cVal = e.target.textContent;
+    if(cVal === "π"){
+        cVal = Math.PI;
+    } else if(cVal === "e"){
+        cVal = Math.E;
+    }
     console.log("Num : " + cVal);
-
     processStack(cVal);
-
     if (cVal != ".") {
         if (!afterOp) {
             result = stack(result, cVal);
@@ -68,34 +71,76 @@ function numChk(e) {
             }
         }
     }
+    console.log("result : " + result + " tempResult : " + tempResult + " op : " + op + " next Op : " + nextOp);
 }
 
-//
+//연산자처리
 function opChk(e) {
     e.preventDefault();
     afterOp = true;
+    console.log("afterOp : " + afterOp);
+    if(e.target.textContent !== "+" && e.target.textContent !== "-" 
+    && e.target.textContent !== "*" && e.target.textContent !== "/"
+    && e.target.textContent !== "%" && e.target.textContent !== "xy"
+    && e.target.textContent !== "x!"){
+        afterOp = false;
+        if(result !== 0){
+            reset();
+            printResult("문법 error, reset");
+        }
+    }
+    console.log("afterOp : " + afterOp);
 
-    if(nextOp !== 0){
+    if (tempResult !== 0 && result === 0) {
+        result = tempResult;
+    }
+    if (nextOp !== 0) {
         result = Number(result);
         nextOp = Number(nextOp);
-        cal(result, nextOp);
+        calBasic(result, nextOp);
         printResult(result);
+        console.log("result : " + result + " tempResult : " + tempResult + " op : " + op + " next Op : " + nextOp);
         op = e.target.textContent;
         processStack(op);
         nextOp = 0;
-    } else {
-        op = e.target.textContent;
-        console.log("op : " + op);
-        processStack(op);
     }
-
-    
-    
-    //console.log("result, op, nextOp : " +result + op + nextOp);
+    op = e.target.textContent;
+    console.log("op : " + op);
+    processStack(op);
 }
 
-//
-function cal(x, y) {
+//"=" 처리
+function equal(e){
+    e.preventDefault();
+    if (tempResult !== 0 && result === 0) {
+        result = tempResult;
+    }
+    
+    if (nextOp !== 0) {
+        result = Number(result);
+        nextOp = Number(nextOp);
+        calBasic(result, nextOp);
+        printResult(result);
+        processStack(`=(${result})`);
+        tempResult = result;
+        result = 0;
+        nextOp = 0;
+        afterOp = false;
+    }
+
+    if(op !== "+" && op !== "-" && op !== "*"
+    && op !== "/" && op !== "%"&& op !== "xy"){
+        result = Number(result);
+        calIntensive(result);
+        printResult(result);
+        processStack(`=(${result})`);
+        result = 0;   
+    }
+    console.log("result : " + result + " tempResult : " + tempResult + " op : " + op + " next Op : " + nextOp);
+}
+
+//2개 연산
+function calBasic(x, y) {
     if (op === "+") {
         result = (x + y).toString();
     } else if (op === "-") {
@@ -104,24 +149,55 @@ function cal(x, y) {
         result = (x * y).toString();
     } else if (op === "/") {
         result = (x / y).toString();
+    } else if (op === "%") {
+        result = (x % y).toString();
+    } else if (op === "xy") {
+        result = ((Math.pow(x,y))).toString();
     }
 }
 
-//
+//1개 연산
+//Math.log는 밑이 e, Math.log10은 밑이 10, 너무깊게 생각x
+function calIntensive(x){
+    if (op === "sin"){
+        result = ((Math.sin(x))).toString();
+    } else if (op === "cos"){
+        result = ((Math.cos(x))).toString();
+    } else if (op === "tan"){
+        result = ((Math.tan(x))).toString();
+    } else if (op === "√"){
+        result = ((Math.sqrt(x))).toString();
+    } else if (op === "log"){
+        result = ((Math.log10(x))).toString();
+    } else if (op === "ln"){
+        result = ((Math.log(x))).toString();
+    } else if (op === "exp"){
+        result = ((Math.exp(x))).toString();
+    } else if (op === "x!"){
+        result = (factorial(x)).toString();
+    }
+}
+
+//펙토리얼 계산(if(0)=false이다?)
+function factorial(n){
+    return n ? n * factorial(n-1) : 1;
+}
+
+//초기화
 function reset(e) {
-    e.preventDefault();
+    //e.preventDefault();
     console.log("리셋");
     result = 0;
+    tempResult = 0;
     nextOp = 0;
     afterOp = false;
-    //count?
     op = "";
     anyStr = "";
     printResult(0);
     printProcess(0);
 }
 
-//
+//시작
 function init() {
     numBtns.forEach(function (e) {
         e.addEventListener('click', numChk);
@@ -130,6 +206,7 @@ function init() {
         opBtn.addEventListener('click', opChk);
     });
     resetBtn.addEventListener('click', reset);
+    equalBtn.addEventListener('click', equal);
 }
 
 init();
