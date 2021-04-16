aa3128cce1444d546b948bc680998945fd01ad6d
k5275645 <k5275645@naver.com>
2021-04-16 17:32:46 +0900

    upbit api append basic data

--- OpenAPI/index.js
+++ OpenAPI/index.js
@@ -1,5 +1,9 @@
 const container = document.querySelector(".container");
 const marketBtn = document.querySelector(".market");
+const krwBtn = document.querySelector(".KRW");
+const btcBtn = document.querySelector(".BTC");
+const usdtBtn = document.querySelector(".USDT");
+const marketInfo = document.querySelector(".market-info");
 
 const marketTable = document.querySelector(".market-table");
 const marketTableBody = marketTable.querySelector("tbody");
@@ -7,12 +11,104 @@ const marketTableBody = marketTable.querySelector("tbody");
 const options = { method: "GET" };
 
 let all = [];
-
 let marketFlag = true;
-
 marketTable.style.display = "none";
 
-marketBtn.addEventListener("click", function (e) {
+function appendTr(item) {
+  const allTr = document.createElement("tr");
+  const td1 = document.createElement("td");
+  const td2 = document.createElement("td");
+  const td3 = document.createElement("td");
+  const infoBtn = document.createElement("button");
+  td1.innerHTML = item.market;
+  td2.innerHTML = item.korean_name;
+  td3.innerHTML = item.english_name;
+  infoBtn.innerHTML = "가격";
+  infoBtn.addEventListener("click", getTickerInfo);
+
+  allTr.appendChild(td1);
+  allTr.appendChild(td2);
+  allTr.appendChild(td3);
+  allTr.appendChild(infoBtn);
+  marketTableBody.appendChild(allTr);
+}
+
+function click(e) {
+  if (e.target.classList.value === "market") {
+    all.forEach((item) => {
+      appendTr(item);
+    });
+  } else if (e.target.classList.value === "KRW") {
+    all.forEach((item) => {
+      const allTr = document.createElement("tr");
+      const td1 = document.createElement("td");
+      const td2 = document.createElement("td");
+      const td3 = document.createElement("td");
+      const infoBtn = document.createElement("button");
+      td1.innerHTML = item.market;
+      td2.innerHTML = item.korean_name;
+      td3.innerHTML = item.english_name;
+      infoBtn.innerHTML = "가격";
+      infoBtn.addEventListener("click", getTickerInfo);
+
+      let splitMarket = item.market.split("-");
+
+      if (splitMarket[0] === "KRW") {
+        allTr.appendChild(td1);
+        allTr.appendChild(td2);
+        allTr.appendChild(td3);
+        allTr.appendChild(infoBtn);
+        marketTableBody.appendChild(allTr);
+      }
+    });
+  } else if (e.target.classList.value === "BTC") {
+    all.forEach((item) => {
+      const allTr = document.createElement("tr");
+      const td1 = document.createElement("td");
+      const td2 = document.createElement("td");
+      const td3 = document.createElement("td");
+      const infoBtn = document.createElement("button");
+      td1.innerHTML = item.market;
+      td2.innerHTML = item.korean_name;
+      td3.innerHTML = item.english_name;
+      infoBtn.innerHTML = "가격";
+      infoBtn.addEventListener("click", getTickerInfo);
+
+      let splitMarket = item.market.split("-");
+
+      if (splitMarket[0] === "BTC") {
+        allTr.appendChild(td1);
+        allTr.appendChild(td2);
+        allTr.appendChild(td3);
+        allTr.appendChild(infoBtn);
+        marketTableBody.appendChild(allTr);
+      }
+    });
+  } else if (e.target.classList.value === "USDT") {
+    all.forEach((item) => {
+      const allTr = document.createElement("tr");
+      const td1 = document.createElement("td");
+      const td2 = document.createElement("td");
+      const td3 = document.createElement("td");
+      const infoBtn = document.createElement("button");
+      td1.innerHTML = item.market;
+      td2.innerHTML = item.korean_name;
+      td3.innerHTML = item.english_name;
+      infoBtn.innerHTML = "가격";
+      infoBtn.addEventListener("click", getTickerInfo);
+
+      let splitMarket = item.market.split("-");
+
+      if (splitMarket[0] === "USDT") {
+        allTr.appendChild(td1);
+        allTr.appendChild(td2);
+        allTr.appendChild(td3);
+        allTr.appendChild(infoBtn);
+        marketTableBody.appendChild(allTr);
+      }
+    });
+  }
+
   if (marketFlag) {
     marketTable.style.display = "block";
     marketFlag = false;
@@ -20,47 +116,40 @@ marketBtn.addEventListener("click", function (e) {
     marketTable.style.display = "none";
     marketFlag = true;
   }
-});
-
-function marketInfoAppend() {
-  all.forEach((item) => {
-    const tr = document.createElement("tr");
-    const td1 = document.createElement("td");
-    const td2 = document.createElement("td");
-    const td3 = document.createElement("td");
-    const infoBtn = document.createElement("button");
-    td1.innerHTML = item.market;
-    td2.innerHTML = item.korean_name;
-    td3.innerHTML = item.english_name;
-    infoBtn.innerHTML = "정보보기";
-    infoBtn.addEventListener("click", getTickerInfo);
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    tr.appendChild(infoBtn);
-    marketTableBody.appendChild(tr);
-  });
 }
+
+marketBtn.addEventListener("click", click);
+krwBtn.addEventListener("click", click);
+btcBtn.addEventListener("click", click);
+usdtBtn.addEventListener("click", click);
 
 function getTickerInfo(e) {
   let market = e.target.parentNode.childNodes[0].innerText;
+  let korean_name = e.target.parentNode.childNodes[1].innerText;
+  let english_name = e.target.parentNode.childNodes[2].innerText;
   fetch(`https://api.upbit.com/v1/ticker?markets=${market}`, options)
     .then((response) => response.json())
     .then((data) => {
       data.forEach((item) => {
+        const div = document.createElement("div");
+        const p = document.createElement("p");
         const ul = document.createElement("ul");
         const li1 = document.createElement("li");
         const li2 = document.createElement("li");
         const li3 = document.createElement("li");
         const li4 = document.createElement("li");
-        li1.innerHTML = item.opening_price;
-        li2.innerHTML = item.trade_price;
-        li3.innerHTML = item.high_price;
-        li4.innerHTML = item.low_price;
+        li1.innerHTML = "시가 : " + item.opening_price;
+        li2.innerHTML = "종가 : " + item.trade_price;
+        li3.innerHTML = "고가 : " + item.high_price;
+        li4.innerHTML = "저가 : " + item.low_price;
+        p.innerHTML = `${market} ${korean_name} ${english_name}`;
         ul.appendChild(li1);
         ul.appendChild(li2);
         ul.appendChild(li3);
         ul.appendChild(li4);
+        div.appendChild(p);
+        div.appendChild(ul);
+        marketInfo.appendChild(div);
       });
     })
     .catch((err) => console.error(err));
@@ -69,14 +158,12 @@ function getTickerInfo(e) {
 function marketInfoLoad() {
   fetch("https://api.upbit.com/v1/market/all?isDetails=false", options)
     .then((response) => {
-      //console.log(response);
       return response.json();
     })
     .then((data) => {
       data.forEach((item) => {
         all.push(item);
       });
-      marketInfoAppend();
     })
     .catch((err) => console.error(err));
 }
