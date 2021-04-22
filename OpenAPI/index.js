ef0f6335bf4a3fb6b54ded55a39a5f0d99e719c4
k5275645 <k5275645@naver.com>
2021-04-22 14:39:45 +0900

    almost,,,

--- OpenAPI/index.js
+++ OpenAPI/index.js
@@ -11,6 +11,41 @@ const options = { method: "GET" };
 let KRW = [];
 let BTC = [];
 let USDT = [];
+let markets = [];
+
+//clearInterval(timer);
+
+async function getItems(mf, flagNum) {
+  const getResponse = await fetch(
+    `https://api.upbit.com/v1/ticker?markets=${mf}`,
+    options
+  );
+  const get = await getResponse.json();
+  const tikerInfo = await get[0];
+  marketTableBody.childNodes[flagNum].childNodes[3].innerText =
+    tikerInfo.opening_price;
+  marketTableBody.childNodes[flagNum].childNodes[4].innerText =
+    tikerInfo.high_price;
+  marketTableBody.childNodes[flagNum].childNodes[5].innerText =
+    tikerInfo.low_price;
+  marketTableBody.childNodes[flagNum].childNodes[6].innerText =
+    tikerInfo.trade_price;
+  return tikerInfo;
+}
+
+function getPrice() {
+  let flagNum = -1;
+  let timer = setInterval(() => {
+    for (i = 0; i < 10; i++) {
+      if (flagNum > markets.length - 2) {
+        flagNum = 0;
+      } else {
+        flagNum++;
+      }
+      getItems(markets[flagNum], flagNum);
+    }
+  }, 1000);
+}
 
 function appendItem(e) {
   let temp;
@@ -25,12 +60,18 @@ function appendItem(e) {
     marketTableBody.removeChild(marketTableBody.childNodes[0]);
   }
 
+  markets.length = 0;
   temp.forEach((item) => {
     const tr = document.createElement("tr");
     const td1 = document.createElement("td");
     const td2 = document.createElement("td");
     const td3 = document.createElement("td");
+    const td4 = document.createElement("td");
+    const td5 = document.createElement("td");
+    const td6 = document.createElement("td");
+    const td7 = document.createElement("td");
 
+    markets.push(item.market);
     td1.innerHTML = item.market;
     td2.innerHTML = item.korean_name;
     td3.innerHTML = item.english_name;
@@ -38,44 +79,19 @@ function appendItem(e) {
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
+    tr.appendChild(td4);
+    tr.appendChild(td5);
+    tr.appendChild(td6);
+    tr.appendChild(td7);
 
     marketTableBody.appendChild(tr);
   });
-  getPrice(temp);
+  getPrice();
 }
 
-function getPrice(temp) {
-  // let timer = setInterval(() => {
-  //   //clearInterval(timer);
-  //   console.log("aa");
-  // }, 1000);
-  for (let i = 0; i < temp.length; i++) {
-    const element = temp[i];
-    console.log(element);
-  }
-  // temp.forEach((item) => {
-  //   console.log(item.market);
-  // });
-  fetch(`https://api.upbit.com/v1/ticker?markets=${market}`, options)
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((i) => {
-        const td4 = document.createElement("tr");
-        const td5 = document.createElement("tr");
-        const td6 = document.createElement("tr");
-        const td7 = document.createElement("tr");
-        td4.innerHTML = i.opening_price;
-        td5.innerHTML = i.high_price;
-        td6.innerHTML = i.low_price;
-        td7.innerHTML = i.trade_price;
-        tr.appendChild(td4);
-        tr.appendChild(td5);
-        tr.appendChild(td6);
-        tr.appendChild(td7);
-      });
-    })
-    .catch((err) => console.error(err));
-}
+krwBtn.addEventListener("click", appendItem);
+btcBtn.addEventListener("click", appendItem);
+usdtBtn.addEventListener("click", appendItem);
 
 function marketInfoLoad() {
   fetch("https://api.upbit.com/v1/market/all?isDetails=false", options)
@@ -84,21 +100,18 @@ function marketInfoLoad() {
     })
     .then((data) => {
       data.forEach((item) => {
-        if (item.market.split("-")[0] === "KRW") {
+        let splitMarket = item.market.split("-");
+        if (splitMarket[0] === "KRW") {
           KRW.push(item);
-        } else if (item.market.split("-")[0] === "BTC") {
+        } else if (splitMarket[0] === "BTC") {
           BTC.push(item);
-        } else if (item.market.split("-")[0] === "USDT") {
+        } else if (splitMarket[0] === "USDT") {
           USDT.push(item);
         }
       });
     })
     .catch((err) => console.error(err));
 }
-
-krwBtn.addEventListener("click", appendItem);
-btcBtn.addEventListener("click", appendItem);
-usdtBtn.addEventListener("click", appendItem);
 
 function init() {
   marketInfoLoad();
